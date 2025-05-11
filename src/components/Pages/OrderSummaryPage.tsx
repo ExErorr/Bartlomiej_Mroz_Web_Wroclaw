b316decc56599fa3954f9591ebe73bd00c1c58a1
ExErorr <kukuryk098@gmail.com>
2025-05-11 18:51:00 +0200

    up

--- src/components/Pages/OrderSummaryPage.tsx
+++ src/components/Pages/OrderSummaryPage.tsx
@@ -3,14 +3,14 @@ import CartContent from "../CartContent";
 import { useCart } from "../../context/CartContext";
 import "./CartPage.css";
 import CartFooter from "../CartFooter";
-import NaviateButton from "../Buttons/NavigateButton";
+import NavigateButton from "../Buttons/NavigateButton";
 
 const OrderSummaryPage: React.FC = () => {
   const { cartItems, isCartEmpty } = useCart();
 
   const handleConfirmOrder = () => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
-    window.location.href = `${import.meta.env.BASE_URL}#/confirmation`;
+    window.location.href = `${import.meta.env.BASE_URL}confirmation`;
   };
 
   return (
@@ -28,7 +28,7 @@ const OrderSummaryPage: React.FC = () => {
           />
         </>
       )}
-      <NaviateButton link="/cart" label="Wróć do koszyka" />
+      <NavigateButton link="/cart" label="Wróć do koszyka" />
     </div>
   );
 };
